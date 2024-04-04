e391f089fc16a4d4073852c3a0bea57f2b67019c
Anastasia Prysiazhna <prysiazhnastasia@gmail.com>
2024-04-04 17:54:24 -0400

    Add format_string

---
+++ format_string.py
@@ -0,0 +1,15 @@
+def format_string(string):
+    """
+    Formats a string by replacing uppercase letters with lowercase letters and spaces with underscores.
+
+    Parameters:
+        string (str): The input string to be formatted.
+
+    Returns:
+        str: The formatted string.
+    """
+    return ''.join('_' if char == ' ' else char.lower() for char in string)
+
+input_string = "File Name"
+formatted_string = format_string(input_string)
+print(formatted_string)
