b8517e8b538f238fc9320df0a2af43d5d1a8ffb2
Anastasia Prysiazhna <prysiazhnastasia@gmail.com>
2024-05-12 11:55:56 +0200

    add implement_stack_using_queues

---
+++ easy/implement_stack_using_queues.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+
+    def pop(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        top_element = self.q1.popleft()
+        self.q1, self.q2 = self.q2, self.q1
+        return top_element
+
+    def top(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        top_element = self.q1[0]
+        self.q2.append(self.q1.popleft())
+        self.q1, self.q2 = self.q2, self.q1
+        return top_element
+
+    def empty(self) -> bool:
+        return not self.q1 and not self.q2
+
+
+# Runtime 30 ms / Memory 16.79 mb
+
+
+# Example 1
+# Input: ["MyStack", "push", "push", "top", "pop", "empty"]
+# [[], [1], [2], [], [], []]
+# Output: [null, null, null, 2, 2, false]
+stack = MyStack()
+stack.push(1)
+stack.push(2)
+print(stack.top())  # Output: 2
+print(stack.pop())  # Output: 2
+print(stack.empty())  # Output: False
--- format_string.py
+++ format_string.py
@@ -11,7 +11,7 @@ def format_string(string):
     return "".join("_" if char == " " else char.lower() for char in string)
 
 
-input_string = "Count Complete Tree Nodes"
+input_string = "Implement Stack using Queues"
 formatted_string = format_string(input_string)
 print(formatted_string)
 
