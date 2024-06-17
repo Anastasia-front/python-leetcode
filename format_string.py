5788cf77a4faa30c6a2d5627e7215ca87f3a2827
Anastasia Prysiazhna <prysiazhnastasia@gmail.com>
2024-06-17 14:28:31 +0200

    add 398_random_pick_index

--- format_string.py
+++ format_string.py
@@ -11,7 +11,7 @@ def format_string(string):
     """
     return "".join("_" if char == " " else char.lower() if char != "." else "" for char in string)
 
-input_string = "399. Evaluate Division"
+input_string = "398. Random Pick Index"
 formatted_string = format_string(input_string)
 print(formatted_string)
 
---
+++ medium/398_random_pick_index.py
@@ -0,0 +1,54 @@
+# Given an integer array nums with possible duplicates, randomly output the index of a given target number. You can assume that the given target number must exist in the array.
+
+# Implement the Solution class:
+
+# Solution(int[] nums) Initializes the object with the array nums.
+# int pick(int target) Picks a random index i from nums where nums[i] == target. If there are multiple valid i's, then each index should have an equal probability of returning.
+
+
+# Example 1:
+
+# Input
+# ["Solution", "pick", "pick", "pick"]
+# [[[1, 2, 3, 3, 3]], [3], [1], [3]]
+# Output
+# [null, 4, 0, 2]
+
+# Explanation
+# Solution solution = new Solution([1, 2, 3, 3, 3]);
+# solution.pick(3); // It should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+# solution.pick(1); // It should return 0. Since in the array only nums[0] is equal to 1.
+# solution.pick(3); // It should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+
+
+# Constraints:
+
+# 1 <= nums.length <= 2 * 104
+# -231 <= nums[i] <= 231 - 1
+# target is an integer from nums.
+# At most 104 calls will be made to pick.
+
+
+import random
+from collections import defaultdict
+
+
+class Solution:
+
+    def __init__(self, nums):
+        self.num_to_indices = defaultdict(list)
+        for i, num in enumerate(nums):
+            self.num_to_indices[num].append(i)
+
+    def pick(self, target):
+        return random.choice(self.num_to_indices[target])
+
+
+# Runtime 287 ms / Memory 26.61 mb
+
+
+# Example usage:
+solution = Solution([1, 2, 3, 3, 3])
+print(solution.pick(3))  # Should return either index 2, 3, or 4 randomly
+print(solution.pick(1))  # Should return 0
+print(solution.pick(3))  # Should return either index 2, 3, or 4 randomly
