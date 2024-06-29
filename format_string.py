4df63bc9ae1574dc8c6428f8c46c5485f2d1ef56
Anastasia Prysiazhna <prysiazhnastasia@gmail.com>
2024-06-29 23:54:52 +0200

    add 382_linked_list_random_node

--- format_string.py
+++ format_string.py
@@ -11,7 +11,7 @@ def format_string(string):
     """
     return "".join("_" if char == " " else char.lower() if char != "." else "" for char in string)
 
-input_string = "384. Shuffle an Array"
+input_string = "382. Linked List Random Node"
 formatted_string = format_string(input_string)
 print(formatted_string)
 
